99a4cc8316f8dcdbe8bf4580d4355b865d322de7
GeorgyAl <georganer02@gmail.com>
2022-05-19 20:57:46 +0300

    Coursework2.7

--- react-fitnes/src/Workout/WorkoutMain/WorkoutMainContent.js
+++ react-fitnes/src/Workout/WorkoutMain/WorkoutMainContent.js
@@ -14,26 +14,63 @@ export default function WorkoutMainContent({ helpActiveClick, store, setStore, s
         });
     }, [user, setUser])
 
+
+
+    useEffect(() => {
+        const fetchData = async () => {
+           const data =  await getDocs (workoutCollectionRef);
+           const allWorkout = (data.docs.map((doc) => ({...doc.data(), id: doc.id})));
+           const newAllWorkout = allWorkout.reduce((prev, item) => {
+               const key = Object.keys(item)[0];
+                console.log(item, 'item')
+               if (prev[key]) {
+                   prev[key] = [
+                       ...prev[key],
+                       ...item[key],
+                       ] 
+               } else {
+                   console.log(item, 'ITEM')
+                   console.log(key, 'KEY')
+                   prev[key] = [
+                       ...item[key],
+                   ]
+               }
+               return prev
+           }, {})
+           setStore((prevStore) => {
+               return {
+                   ...prevStore,
+                   WorkAllDays: newAllWorkout,
+               }
+           })
+           console.log(newAllWorkout, 'newAllWorkout')
+           console.log(allWorkout, 'allWorkout')
+        }
+        fetchData();
+    }, [])
+
     const workoutCollectionRef = collection(db, 'WorkAllDays');
+
 
     const createWork = async () => {
         await addDoc (workoutCollectionRef, {[selectData]: store.WorkAllDays[selectData], author: {id: auth.currentUser.uid}})
     }
     const getWork = async () => {
-            const data = await getDocs (workoutCollectionRef);
+            /*const data = await getDocs (workoutCollectionRef);
             const works = (data.docs.map((doc) => ({...doc.data(), id: doc.id})));
-            const worker = works.filter((work) => work.author.id === user.uid);
-            if( worker.length === 0) {
+            const worker = store.WorkAllDays.filter((work) => work.author.id === user.uid);*/
+            const worker = store.WorkAllDays[selectData];
+            if(worker.length === 0) {
                 createWork();
             } else {
                 for (let i = 0; i < worker.length; i++) {
-                    if(worker[i].id && worker[i][selectData]) {
+                    if(worker[i][selectData]) {
+                        console.log(worker[i], 'worker[i]')
+                        console.log(worker[i][selectData], 'Worker');
                         let id = worker[i].id;
                         await deleteDoc (doc(db, 'WorkAllDays', `${id}`))
                         createWork();
-                    } else {
-                        createWork();
-                    }
+                    } 
                 }
             }
     };
@@ -42,7 +79,7 @@ export default function WorkoutMainContent({ helpActiveClick, store, setStore, s
         e.preventDefault();
         getWork();
     }
-
+    console.log(store.WorkAllDays, 'общий стор')
     return <div className="workout">
                 <ul className="workout_item_head">
                     <li className="workout_item" onClick={helpActiveClick}>Добавить<br/>упражнение</li>
